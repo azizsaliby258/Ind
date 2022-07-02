4f9010d17cc9b76bcd38bab325dffac51e112901
azizsaliby258 <108574245+azizsaliby258@users.noreply.github.com>
2022-07-02 15:15:46 +0300

    Update suicide.py

--- suicide.py
+++ suicide.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
-import seaborn as sns
 import plotly.graph_objects as go
+import seaborn as sns
 import plotly.express as px
 import matplotlib.pyplot as plt 
 
